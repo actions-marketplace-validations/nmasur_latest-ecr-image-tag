9dcddd4f412e7b59fdf39c2979420f719ba9bbc6
Noah Masur <noah.masur@take2games.com>
2021-03-10 17:23:09 -0500

    use inputs instead of env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -l
 
-hash=$(aws ecr describe-images --repository-name $REPOSITORY_NAME | jq '.imageDetails | .[].imageTags | select(index( "latest" )) | .[] | select(. != "latest")')
+AWS_ACCESS_KEY_ID=$1
+AWS_SECRET_ACCESS_KEY=$2
+AWS_REGION=$3
+REPOSITORY_NAME=$4
+
+hash=$(AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws ecr describe-images --repository-name $REPOSITORY_NAME --region $AWS_REGION | jq '.imageDetails | .[].imageTags | select(index( "latest" )) | .[] | select(. != "latest")')
 echo "::set-output name=hash::$hash"
